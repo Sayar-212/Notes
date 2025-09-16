c4347fa5de8c137ec0bc6acf3df6f29a36301706
HITK <hitkconfessions8@gmail.com>
2025-09-16 16:01:55 +0530

    Update trigonometric ratios exercise link to new Dropbox PDF

--- dashboard.js
+++ dashboard.js
@@ -4,7 +4,7 @@ function openCourse(courseId) {
             window.open('https://online.flippingbook.com/view/141264682/', '_blank');
             break;
         case 'trigonometry-exercise':
-            window.open('https://ucd4e520245339c50849a1876aa7.dl.dropboxusercontent.com/cd/0/inline2/Cxfa8jtZFvbUk8niwNRk6uv27wJjbcOk-s-ptLWYFmR-2eeC8-eV36K3qdfxwnSF4ZmEnBuDkGiZwuaZG1uxbFjNs7xZf0drU98LcT_CBUy6ix7ZYDeLHdhJXJQcqHGv346w16JkmxBf0IrHg-vRriG5iISAAy4eO24J0oT2ijZNX5SDhSl-1rAIfdK1wOekNF7edvyqApkBA0TxjusTxEajHrE09_i0PwgxXhM9ZEB8QLlrhGcfVN7fj3pI_SSTwp_1PQxUreJMDkI00tCvOjfNHVwt61r6tCpQ9jmkRjekfoKcHyk65TcOPMSqjyTSCIZ1ErrlikvSk4IFPZi-Qram8KwfZHhTPHxMeALoMZTcPRVIuoBn9i15ELNntRabSWM/file', '_blank');
+            window.open('https://www.dropbox.com/scl/fi/26by4m7nhgsfna1pffi63/Trigonometric_Ratios_Exercise_1.pdf?rlkey=9xs7mfha46fmc7lh9vwxdto1v&st=6zza5eoe&raw=1', '_blank');
             break;
         case 'algebra':
             alert('Opening Linear Algebra course...');
